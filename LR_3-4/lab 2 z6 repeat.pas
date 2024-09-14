bbd9f252291974d09debf8ee91480c7c40bbdaff
matthewqx13 <144227151+matthewqx13@users.noreply.github.com>
2024-09-14 11:09:25 +0300

    Add files via upload

---
+++ LR_3-4/lab 2 z6 repeat.pas
@@ -0,0 +1,18 @@
+﻿program z6;
+var
+  num, count, i:int64;
+  
+  begin
+    write('Введите натуральное число: ');
+    readln(num);
+    
+    count:=0;
+    i:=1;
+
+    repeat
+    if num mod i = 0 then count := count + 1;
+    i := i + 1;
+  until i > num;
+    
+    writeln('Кол-во делителей числа ', count);
+  end.
